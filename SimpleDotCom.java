93f200c3b1a38cbea38be78415c72a9e85a22f16
Vineela Kodamagundla <vineelakodamagundla2812@gmail.com>
2022-01-18 21:22:18 +0530

    Finished SimpleDotCom class

--- SimpleDotCom.java
+++ SimpleDotCom.java
@@ -1,16 +1,23 @@
-public String CheckYourself(String stringGuess){
-	int guess = Integer.parseInt(stringGuess);  //converting String to int
-	String result = "miss";		//setting default result as miss
-	for(int cell : locationCells){		//chech for each cell in the locationCells array
-		if(guess == cell){		//if the guess is correct
-			result = "hit";		//change the result to 'hit'
-			numOfHits++;		//increment the numOfHits
-			break;				//comeout of the loop
+public class SimpleDotCom{
+	int [] locationCells;
+	int numOfHits=0;		//initialise numOfHits to 0
+	public void setlocationCells(int[]lc){
+		locationCells = lc;			//set locationCells 
+	}
+	public String CheckYourself(String stringGuess){
+		int guess = Integer.parseInt(stringGuess);  //converting String to int
+		String result = "miss";		//setting default result as miss
+		for(int cell : locationCells){		//chech for each cell in the locationCells array
+			if(guess == cell){		//if the guess is correct
+				result = "hit";		//change the result to 'hit'
+				numOfHits++;		//increment the numOfHits
+				break;				//comeout of the loop
+			}
 		}
+		if(numOfHits == locationCells.length){		//if all cells in locationCells array are guessed
+			result = "kill";				//set the result to kill
+		}
+		System.out.println(result);			//print the result
+		return result;						//return the result
 	}
-	if(numOfHits == locationCells.length){		//if all cells in locationCells array are guessed
-		result = "kill";				//set the result to kill
-	}
-	System.out.println(result):			//print the result
-	return result;						//return the result
 }
